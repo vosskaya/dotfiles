4d3f76dd222f03f46626620e786146d146f0939f
Volker Voßkämper <vvo@cenote.de>
2024-12-27 17:29:15 +0100

    fixup! Add local installed fzf from vim/nvim plugins to PATH if needed

--- home/dot_config/nvim/lua/plugins/fzf.lua
+++ home/dot_config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,18 @@
 return {
   "junegunn/fzf",
-  -- We only need the fzf binary here
-  enabled = vim.fn.executable("fzf") == 0, -- Disable if `fzf` is already available
-  build = "./install --bin", -- Build if enabled
+--   -- We only want the fzf binary from this plugin if not installed elsewhere
+  enabled = function()
+    -- Check for a system-wide `fzf` binary
+    local system_fzf = vim.fn.exepath("fzf")
+
+    -- Compute the path to the `fzf` binary provided by this plugin
+    local plugin_fzf = vim.fn.stdpath("data") .. "/lazy/fzf/bin/fzf"
+
+    -- Enable the plugin if:
+    -- 1. No system-wide `fzf` binary exists
+    -- 2. The `fzf` binary in `PATH` is the one provided by this plugin
+    return not system_fzf or system_fzf == plugin_fzf
+  end,
+  build = "./install --bin", -- Build the binary if enabled
 }
 
