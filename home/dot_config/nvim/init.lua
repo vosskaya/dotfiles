3b1fc9bb2577add7144e73981692a3a1b20ceb35
Volker Voßkämper <vvo@cenote.de>
2024-12-31 00:34:43 +0100

    Added reading init_local.lua if present

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- local additions
 require("config.lazy")
+local local_init = vim.fn.stdpath("config") .. "init_local.lua"
+if vim.fn.filereadable(local_init) == 1 then
+  dofile(local_init)
+end
