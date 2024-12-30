cb79d97bdb11197c098a1cf4aee05683e0db2dd4
Volker Voßkämper <vvo@cenote.de>
2024-12-31 00:39:52 +0100

    fixup! Added reading init_local.lua if present

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- local additions
 require("config.lazy")
-local local_init = vim.fn.stdpath("config") .. "init_local.lua"
+local local_init = vim.fn.stdpath("config") .. "/init_local.lua"
 if vim.fn.filereadable(local_init) == 1 then
   dofile(local_init)
 end
