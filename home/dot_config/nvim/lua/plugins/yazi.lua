fe9b0e792d2274e657584f3f2eea9cba3bc616ef
Volker Voßkämper <vvo@cenote.de>
2024-12-31 21:05:55 +0100

    Added yazi.nvim

---
+++ home/dot_config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,39 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+-- if true then return {} end
+
+return
+---@type LazySpec
+{
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>fy",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>fY",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory" ,
+    },
+    -- {
+    --   -- NOTE: this requires a version of yazi that includes
+    --   -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+    --   '<c-up>',
+    --   "<cmd>Yazi toggle<cr>",
+    --   desc = "Resume the last yazi session",
+    -- },
+  },
+  ---@type YaziConfig
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
+}
